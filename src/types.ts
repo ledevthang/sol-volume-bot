e085da8846ad665d36d9881b455be3f55dbe8327
lainhathoang <nhathoang8c01@gmail.com>
2024-11-26 09:53:18 +0700

    feat: add trading feature

---
+++ src/types.ts
@@ -0,0 +1,45 @@
+import type { Connection, Keypair } from "@solana/web3.js"
+
+export type VolumeBotConfig = {
+	inputMint: string
+	outputMint: string
+	numberOfOrders: number
+	minQuantity: number
+	maxQuantity: number
+	interval: number
+}
+
+export type SwapParams = {
+	connection: Connection
+	owner: Keypair
+	inputMint: string
+	outputMint: string
+	amountIn: number //
+	slippage: number // 0.5 => 0.5%
+}
+
+export interface SwapCompute {
+	id: string
+	success: true
+	version: "V0" | "V1"
+	openTime?: undefined
+	msg: undefined
+	data: {
+		swapType: "BaseIn" | "BaseOut"
+		inputMint: string
+		inputAmount: string
+		outputMint: string
+		outputAmount: string
+		otherAmountThreshold: string
+		slippageBps: number
+		priceImpactPct: number
+		routePlan: {
+			poolId: string
+			inputMint: string
+			outputMint: string
+			feeMint: string
+			feeRate: number
+			feeAmount: string
+		}[]
+	}
+}
